ee752d995bde56abd3213a017aaf553b52e4a84a
Matthew Shoemaker <Matthew.Shoemaker@safelite.com>
2019-08-08 12:03:14 -0400

    tests

--- SamuraiDojo.Test/ClockAnglerTest/ClockAnglerTestBase.cs
+++ SamuraiDojo.Test/ClockAnglerTest/ClockAnglerTestBase.cs
@@ -15,7 +15,7 @@ namespace SamuraiDojo.Test.ClockAnglerTest
             int angle = Calculate(12, 15);
             int expected = 90;
 
-            Assert.IsTrue(angle == expected || angle == 360 - expected, $"Expected {expected} or {360- expected} but found {angle}");
+            AssertAngle(expected, angle);
         }
 
         [TestMethod]
@@ -24,7 +24,7 @@ namespace SamuraiDojo.Test.ClockAnglerTest
             int angle = Calculate(12, 0);
             int expected = 0;
 
-            Assert.IsTrue(angle == expected || angle == 360 - expected, $"Expected {expected} or {360 - expected} but found {angle}");
+            AssertAngle(expected, angle);
         }
 
         [TestMethod]
@@ -33,7 +33,7 @@ namespace SamuraiDojo.Test.ClockAnglerTest
             int angle = Calculate(12, 30);
             int expected = 180;
 
-            Assert.IsTrue(angle == expected || angle == 360 - expected, $"Expected {expected} or {360 - expected} but found {angle}");
+            AssertAngle(expected, angle);
         }
 
         [TestMethod]
@@ -42,7 +42,7 @@ namespace SamuraiDojo.Test.ClockAnglerTest
             int angle = Calculate(12, 45);
             int expected = 270;
 
-            Assert.IsTrue(angle == expected || angle == 360 - expected, $"Expected {expected} or {360 - expected} but found {angle}");
+            AssertAngle(expected, angle);
         }
 
         [TestMethod]
@@ -51,7 +51,7 @@ namespace SamuraiDojo.Test.ClockAnglerTest
             int angle = Calculate(3, 30);
             int expected = 90;
 
-            Assert.IsTrue(angle == expected || angle == 360 - expected, $"Expected {expected} or {360 - expected} but found {angle}");
+            AssertAngle(expected, angle);
         }
 
         [TestMethod]
@@ -60,7 +60,7 @@ namespace SamuraiDojo.Test.ClockAnglerTest
             int angle = Calculate(3, 45);
             int expected = 180;
 
-            Assert.IsTrue(angle == expected || angle == 360 - expected, $"Expected {expected} or {360 - expected} but found {angle}");
+            AssertAngle(expected, angle);
         }
 
         [TestMethod]
@@ -69,7 +69,7 @@ namespace SamuraiDojo.Test.ClockAnglerTest
             int angle = Calculate(6, 30);
             int expected = 0;
 
-            Assert.IsTrue(angle == expected || angle == 360 - expected, $"Expected {expected} or {360 - expected} but found {angle}");
+            AssertAngle(expected, angle);
         }
 
         [TestMethod]
@@ -78,7 +78,7 @@ namespace SamuraiDojo.Test.ClockAnglerTest
             int angle = Calculate(6, 15);
             int expected = 90;
 
-            Assert.IsTrue(angle == expected || angle == 360 - expected, $"Expected {expected} or {360 - expected} but found {angle}");
+            AssertAngle(expected, angle);
         }
 
         [TestMethod]
@@ -87,7 +87,7 @@ namespace SamuraiDojo.Test.ClockAnglerTest
             int angle = Calculate(9, 15);
             int expected = 180;
 
-            Assert.IsTrue(angle == expected || angle == 360 - expected, $"Expected {expected} or {360 - expected} but found {angle}");
+            AssertAngle(expected, angle);
         }
 
         [TestMethod]
@@ -96,8 +96,68 @@ namespace SamuraiDojo.Test.ClockAnglerTest
             int angle = Calculate(9, 0);
             int expected = 90;
 
+            AssertAngle(expected, angle);
+        }
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+            int angle = Calculate(1, 0);
+            int expected = 30;
+
+            AssertAngle(expected, angle);
+        }
+
+        [TestMethod]
+        public void TestMethod12()
+        {
+            int angle = Calculate(2, 30);
+            int expected = 105;
+
+            AssertAngle(expected, angle);
+        }
+
+        [TestMethod]
+        public void TestMethod13()
+        {
+            int angle = Calculate(7, 0);
+            int expected = 150;
+
+            AssertAngle(expected, angle);
+        }
+
+        [TestMethod]
+        public void TestMethod14()
+        {
+            int angle = Calculate(10, 30);
+            int expected = 135;
+
+            AssertAngle(expected, angle);
+        }
+
+        [TestMethod]
+        public void TestMethod15()
+        {
+            int angle = Calculate(11, 20);
+            int expected = 140;
+
+            AssertAngle(expected, angle);
+        }
+
+        [TestMethod]
+        public void TestMethod16()
+        {
+            int angle = Calculate(3, 40);
+            int expected = 130;
+
+            AssertAngle(expected, angle);
+        }
+
+        private void AssertAngle(int expected, int angle)
+        {
             Assert.IsTrue(angle == expected || angle == 360 - expected, $"Expected {expected} or {360 - expected} but found {angle}");
         }
+
 
 
     }
