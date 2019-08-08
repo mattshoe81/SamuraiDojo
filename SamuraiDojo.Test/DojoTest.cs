1d6090c57a5489072a9ed1308761ab720e742e59
Matthew Shoemaker <shoemaker.277@osu.edu>
2019-08-08 15:48:51 -0400

    Implementing Score keeper. Incomplete

---
+++ SamuraiDojo.Test/DojoTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiDojo.Test
+{
+    public abstract class DojoTest
+    {
+        protected abstract void RegisterTests();
+        protected abstract void RegisterTestClass();
+    }
+}
