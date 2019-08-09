4bf39eef7bbf7264aa1697e96ce9ff0694aaa1ea
Dustin Mattox <dustin.mattox@safelite.com>
2019-08-09 12:21:54 -0400

    Dustin week 1 solution

---
+++ SamuraiDojo.Test/ClockAnglerTest/Dustin.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SamuraiDojo.Challenges;
+using System;
+
+
+namespace SamuraiDojo.Test.ClockAnglerTest
+{
+    [TestClass]
+    public class Dustin : ClockAnglerTestBase
+    {
+        protected override int Calculate(int hour, int minute)
+        {
+            return ClockAngler.Dustin_CalculateAngleBetweenHands(hour, minute);
+        }
+    }
+}
