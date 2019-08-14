eb9fb6d5ff25ff875f9ca7bc14dd22feaf9bd20c
Dustin <barch54@gmail.com>
2019-08-13 21:39:36 -0400

    Week 2 initial
    Initial solution

---
+++ SamuraiDojo/Challenges/Week2/Dustin.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SamuraiDojo.Attributes;
+
+namespace SamuraiDojo.Challenges.Week2
+{
+    [WrittenBy(Samurai.DUSTIN)]
+    public class Dustin : ICharacterCounter
+    {
+        public int CountPossibleCharacters(string integers)
+        {
+            int permutations = 0;
+
+            string checkDoubleDigit = "";
+            string checkSingleDigit = "";
+            bool addToPermutations = false;
+
+
+
+            for (int i = 0; i < integers.Length; i++)
+            {
+                if (integers.Substring(i, 1) == "0")
+                {
+
+                }
+                else if ((integers.Substring(i, 1) == "1" || integers.Substring(i, 1) == "2") && i != integers.Length - 1)
+                {
+
+                    checkSingleDigit = integers.Substring(i, 1);
+                    addToPermutations = IsValidPermutation(checkSingleDigit);
+
+                    if (addToPermutations)
+                    {
+                        permutations++;
+                        addToPermutations = false;
+                    }
+
+                    checkDoubleDigit = integers.Substring(i, 2);
+                    addToPermutations = IsValidPermutation(checkDoubleDigit);
+
+                    if (addToPermutations)
+                    {
+                        permutations++;
+                        addToPermutations = false;
+                    }
+
+                }
+                else
+                {
+                    checkSingleDigit = integers.Substring(i, 1);
+                    addToPermutations = IsValidPermutation(checkSingleDigit);
+
+                    if (addToPermutations)
+                    {
+                        permutations++;
+                        addToPermutations = false;
+                    }
+                }
+            }
+
+
+            return permutations;
+        }
+
+
+        public bool IsValidPermutation(string input)
+        {
+            bool successfulParse = false;
+            int intSingleDigit = 0;
+
+            successfulParse = Int32.TryParse(input, out intSingleDigit);
+
+            if (successfulParse && intSingleDigit >= 1 && intSingleDigit <= 26)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+}
