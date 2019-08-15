96317004b16d67499b5d79f1c874acdbc24e36aa
AaronPetrySafeLite <51921471+AaronPetrySafeLite@users.noreply.github.com>
2019-08-15 14:33:42 -0400

    Week 2 Solution

---
+++ SamuraiDojo/Challenges/Week2/Aaron.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SamuraiDojo.Attributes;
+
+namespace SamuraiDojo.Challenges.Week2
+{
+    [WrittenBy(Samurai.Aaron)]
+    public class Aaron : ICharacterCounter
+    {
+        public int CountPossibleCharacters(string integers)
+        {
+            var validCharCount = 0;
+            if(integers.Length == 0) { return 0; }
+            const int CharmapShift = 64; //The first 64 characters in ASCII are non-alpha. A == 65.
+            for (int i = 0; i < integers.Length; i++)
+            {
+                int single = int.Parse(integers.Substring(i, 1));
+                if (single <= 0)
+                {
+                    continue;
+                }
+                var singleChar = (char)(single + CharmapShift);
+                validCharCount++;
+
+                if (i >= integers.Length - 1)
+                {
+                    continue;
+                }
+                int doub = int.Parse(integers.Substring(i, 2));
+                var doubChar = (char)(doub + CharmapShift);
+                if (0 < doub && doub <= 26) { validCharCount++; }
+            }
+
+            return validCharCount;
+        }
+    }
+}
