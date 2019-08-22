c1d222ec34cb7e3f9b23499f805baeb8bb53f162
Dustin Mattox <dustin.mattox@safelite.com>
2019-08-22 10:32:02 -0400

    Dustin solution week 3

---
+++ SamuraiDojo/Battles/Week3/Dustin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using SamuraiDojo.Attributes;
+
+namespace SamuraiDojo.Battles.Week3
+{
+    [WrittenBy(Samurai.DUSTIN)]
+    public class Dustin : CensusMaximus
+    {
+        public override int MostPopulousYear(List<Person> people)
+        {
+            int mostPopulousYear = 0;
+            int mostPopulousYearPopulation = 0;
+            int peopleAliveCheck = 0;
+
+            for (int i = 1900; i <= 2000; i++)
+            {
+                foreach (Person p in people)
+                {
+                    if (i >= p.Born && i <= p.Deceased)
+                    {
+                        peopleAliveCheck++;
+                    }
+                }
+
+                if (peopleAliveCheck > mostPopulousYearPopulation)
+                {
+                    mostPopulousYearPopulation = peopleAliveCheck;
+                    mostPopulousYear = i;
+                }
+
+                peopleAliveCheck = 0;
+
+            }
+
+            return mostPopulousYear;
+        }
+    }
+}
