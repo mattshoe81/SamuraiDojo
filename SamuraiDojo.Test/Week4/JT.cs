d71a3ae2628101bd67f1db8f13ea4f98c000a3c1
Unknown <jonathan.turner@safelite.com>
2019-08-31 18:59:53 -0400

    week4 JT solution
    fun times

---
+++ SamuraiDojo.Test/Week4/JT.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SamuraiDojo.Attributes;
+using SamuraiDojo.Battles.Week4;
+
+namespace SamuraiDojo.Test.Week4
+{
+    [TestClass]
+    [WrittenBy(Samurai.JT)]
+    public class JT : PalindromaniaTest
+    {
+        protected override Palindromania GetInstance()
+        {
+            return new SamuraiDojo.Battles.Week4.JT();
+        }
+    }
+}
---
+++ SamuraiDojo/Battles/Week4/JT.cs
@@ -0,0 +1,33 @@
+﻿using SamuraiDojo.Attributes;
+using System.Linq;
+using System;
+
+namespace SamuraiDojo.Battles.Week4
+{
+    [WrittenBy(Samurai.JT)]
+    public class JT : Palindromania
+    {
+        public override string LargestPalindrome(string input)
+        {
+            input = input.ToLower();
+            string largestPalindrome = input[0].ToString();
+            int palStartIndex = 0;
+            string reversedInput = String.Join("", input.Reverse());
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                string substr = input.Substring(palStartIndex, largestPalindrome.Length + 1);
+                if (reversedInput.Contains(substr))
+                {
+                    largestPalindrome = substr;
+                }
+                else
+                {
+                    palStartIndex++;
+                    if (input.Length - palStartIndex < largestPalindrome.Length) break;
+                }
+            }
+            return largestPalindrome;
+        }
+    }
+}
