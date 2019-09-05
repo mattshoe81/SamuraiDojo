5797728c3a349bf8b3ce646864e9f9c079c102b5
Unknown <jonathan.turner@safelite.com>
2019-09-05 17:36:39 -0400

    JT Week 5
    screw your data structures

---
+++ SamuraiDojo/Battles/Week5/JT.cs
@@ -0,0 +1,11 @@
+﻿using SamuraiDojo.Attributes;
+using System.Linq;
+
+namespace SamuraiDojo.Battles.Week5
+{
+    [WrittenBy(Samurai.JT)]
+    public class JT : Snowflake
+    {
+        public override char FindFirstUniqueCharacter(string input) => input.GroupBy(x => x).Where(g => g.Count() == 1).Select(g => g.Key).FirstOrDefault();
+    }
+}
