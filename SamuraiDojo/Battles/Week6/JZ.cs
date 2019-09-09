84ce110179b4586be3d84746b251204c65880b4a
Jeremy-Z <Jeremy.Zimmerman@safelite.com>
2019-09-09 14:09:24 -0400

    JZ's Solution

---
+++ SamuraiDojo/Battles/Week6/JZ.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SamuraiDojo.Attributes;
+
+namespace SamuraiDojo.Battles.Week6
+{
+    [WrittenBy(Samurai.JEREMY_ZIMMERMAN)]
+    public class JZ : SuperfluousSansLoop
+    {
+        public override string Print1toNWithoutLoops(int n)
+        {
+            if (n == 0)
+                return n.ToString();
+            else
+                return Print1toNWithoutLoops(n - 1) + ", " + n.ToString();
+        }
+    }
+}
